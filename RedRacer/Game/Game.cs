9e764f6d617fc3dcd71b57564fee8877baa6b9c4
Reda Baydoun <reda.baydoun@gmail.com>
2017-02-12 21:56:25 -0500

    Basic project files

---
+++ RedRacer/Game/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedRacer.Game
+{
+
+
+	class Game : IGame
+	{
+		private readonly IInput inputMngr;
+		private readonly IRenderer renderMngr;
+
+		public Game(IInput inputMngr, IRenderer renderMngr)
+		{
+			this.inputMngr = inputMngr;
+			this.renderMngr = renderMngr;
+		}
+	}
+}
