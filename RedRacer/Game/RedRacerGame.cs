53bf06d4d4d48d8ed89794b1490e4c1d1a3b3772
Reda Baydoun <reda.baydoun@gmail.com>
2017-04-14 11:09:12 -0400

    Restructures a few things

---
+++ RedRacer/Game/RedRacerGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedRacer.Game
+{
+
+    // The game engine.
+	class RedRacerGame : IGame
+	{
+
+		public RedRacerGame()
+		{
+
+    }
+ 
+    public void Update(int dt)
+    {
+
+    }
+	}
+}
