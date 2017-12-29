8650c2b7b7a161b7883b1222eb8219e73f4331b7
Reda Baydoun <reda.baydoun@gmail.com>
2017-12-29 18:05:47 -0500

    RR Initial commit for RedRacerRoadRenderer, also cleaned up some unused dependencies

---
+++ RedRacer/Game/RedRacerRoadRenderer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RedRacer.Game
+{
+  class RedRacerRoadRenderer : AbstractRenderer
+  {
+
+    public RedRacerRoadRenderer() : base()
+    {
+      Init();
+    }
+
+    protected override void Init()
+    {
+      // Nothing to initialize.
+      IsInitialized = true;
+    }
+
+    public override void RenderToFrameBuffer(byte[] buffer, int width, int height)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override void RenterToFrameBuffer(byte[] buffer, int width, int height, IGameState gameState)
+    {
+      if (IsInitialized)
+      {
+        // Render the road here.
+
+      }
+    }
+  }
+}
